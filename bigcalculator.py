29884370e0f09701ef2b54f202801e700f14f8bb
YavuzMollahamzaoglu <yavuz.mersin@hotmail.com>
2021-05-14 02:27:24 +0300

    add variables examples

--- bigcalculator.py
+++ bigcalculator.py
@@ -2,14 +2,14 @@ def calculate():
     num1 = float(input('Enter first number:'))
     num2 = float(input('Enter second number:'))
 
-    op = input('Enter operator:')
-    if op == '+':
+    op = input('1 = Addition\n2 = Subtraction\n3 = Division\n4 = Multiplication\n Enter Operator: ')
+    if op == '1':
         addition(num1, num2)
-    elif op == '-':
+    elif op == '2':
         subtraction(num1, num2)
-    elif op == '/':
+    elif op == '3':
         division(num1, num2)
-    elif op == '*':
+    elif op == '4':
         multiplication(num1, num2)
     else:
         print('Invalid Operator')
---
+++ değşkenler.py
@@ -0,0 +1,17 @@
+gün = 22
+
+gidiş_ücreti = 1.5
+
+dönüş_ücreti = 1.4
+
+print(gün * (gidiş_ücreti + dönüş_ücreti))
+print(gün * 12)     # yıllık
+
+
+çap = 16
+yarıçap = çap / 2
+pi = 3.14159
+
+alan = pi* (yarıçap*yarıçap)
+print(alan)
+
