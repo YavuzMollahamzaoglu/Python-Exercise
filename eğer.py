3a169c352c447fb2ad4825d326aad55f8d5ceceb
YavuzMollahamzaoglu <yavuz.mersin@hotmail.com>
2021-05-19 10:43:29 +0300

    if statement with exercises

---
+++ eğer.py
@@ -0,0 +1,13 @@
+age = int(input('Your Age :'))
+
+if age == 18:
+    print('18 is a good time!')
+
+elif age < 0:
+    print('I can not believe this .')
+
+elif age < 18:
+    print(' You are younger than me .')
+
+elif age > 18:
+    print('Bro it is huge I was not exceot that :D .')
