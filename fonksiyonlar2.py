45ad032b9fb4af3d4a48755fecafae801edc8538
YavuzMollahamzaoglu <yavuz.mersin@hotmail.com>
2021-05-22 06:36:51 +0300

    functions exercises

---
+++ fonksiyonlar2.py
@@ -0,0 +1,44 @@
+
+'''def factoriel(number):                #tanımlamayı sağlar
+    factoriel = 1
+    for i in range(1, number+1):
+        factoriel *=i
+        print('Factoriel:',factoriel)
+
+
+
+num = int(input('Enter Number:'))
+
+
+factoriel(num)'''
+
+'''def hello_func(greeting,name = 'You'):
+    return'{}, {} Function.'.format(greeting)
+
+print(hello_func('Hi'))'''
+
+'''def merhaba(parametre):
+    print('merhaba'+ str(parametre))
+merhaba('Yavuz')
+merhaba(10)'''
+
+
+import random
+
+def make_num(start=0, finish=500, piece=6)
+    numbers = set()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
